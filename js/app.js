1d5edfeaf6429f1dd67b3c9a3c5e5509e7d94b2b
Jess Moran <j-es-itza@hotmail.com>
2018-01-30 12:01:05 -0600

    actualizando js

--- js/app.js
+++ js/app.js
@@ -27,17 +27,12 @@ function authentication(provider){
     // ...
   }).catch(function(error) {
     // Handle Errors here.
-    console.log(error);
     var errorCode = error.code;
-    console.log(errorCode);
     var errorMessage = error.message;
-    console.log(errorMessage);
     // The email of the user's account used.
     var email = error.email;
-    console.log(email);
     // The firebase.auth.AuthCredential type that was used.
     var credential = error.credential;
-    console.log(credential);
     // ...
   });
 
