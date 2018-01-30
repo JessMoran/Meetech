0242ba046b14124d4dc2ad25c3afc3b78ca797f5
Jess Moran <j-es-itza@hotmail.com>
2018-01-30 09:35:50 -0600

    Actualizando

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyCWfz5QVHhdbaYA0qZxhrk74Dqv4cd3yZ4",
+  authDomain: "meetch-19dcf.firebaseapp.com",
+  databaseURL: "https://meetch-19dcf.firebaseio.com",
+  projectId: "meetch-19dcf",
+  storageBucket: "",
+  messagingSenderId: "738275435756"
+};
+firebase.initializeApp(config);
